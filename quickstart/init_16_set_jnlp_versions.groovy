f4685758fb8514fee41444766eccf21d475fd51f
Andreas Skarmutsos Lindh <andreas@superblock.se>
2017-07-01 03:45:33 +0200

    Behave like other scripts in quickstart

--- quickstart/init_16_set_jnlp_versions.groovy
+++ quickstart/init_16_set_jnlp_versions.groovy
@@ -1,12 +1,21 @@
 import java.util.logging.Logger
 import jenkins.model.*
 
-Logger logger = Logger.getLogger("init.d init_16_set_jnlp_versions.groovy")
+def configName = "init_16_set_jnlp_versions.groovy"
+Logger logger = Logger.getLogger("$configName")
 
 Set enabledProtocols = [
     "Ping",
     "JNLP4-connect"
 ]
-logger.info("Setting accepted JNLP protocols to: " + enabledProtocols)
 
+File disableSaveScript = new File(Jenkins.getInstance().getRootDir(), ".disable-$configName")
+if (disableSaveScript.exists()) {
+    logger.info("DISABLED $configName")
+    return
+}
+
+logger.info("Setting accepted JNLP protocols to: " + enabledProtocols)
 Jenkins.instance.setAgentProtocols(enabledProtocols)
+
+disableSaveScript.createNewFile()
